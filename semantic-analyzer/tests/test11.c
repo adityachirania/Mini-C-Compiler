0942089cdd4837b78121272d10e7281b843405e8
adityasohoni <adityasohoni@gmail.com>
2020-10-12 10:19:10 +0530

    Added test cases 11 and 12 to semantic analyzer

---
+++ semantic-analyzer/tests/test11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    long int a, b;
+    unsigned long int x;
+    signed short int y;
+    signed short z;
+    int w;
+    a = 23;
+    b = 15;
+    a=a+b;
+    b-=a;
+}
---
+++ semantic-analyzer/tests/test12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int c=0;
+    int a[3]={1,2,3};
+    while(c>0) {
+        c--;
+    }
+    int d=50;
+    int i=0;
+    for(i=0;i<d;i++)
+    	d--;
+    
+}
